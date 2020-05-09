1c7181ff7ca4238915f72b5de350f8c5877108b4
Daniel Cole <dancole@Daniels-MacBook.local>
2020-05-08 22:05:24 -0500

    other files

---
+++ src/Contact.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+    <p>This information is private. Sorry!</p>
+  </div>
+)
---
+++ src/NoMatch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NoMatch = () => (
+  <div>
+    <h2>404 Page Not Found</h2>
+    <p>You are here. Who knows where anything else is.</p>
+  </div>
+)
